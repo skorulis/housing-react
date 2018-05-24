be82a51409cdecaae4a79f1222087866491775d0
Alex Skorulis <skorulis@gmail.com>
2018-05-25 07:22:23 +1000

    Create list of suburbs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SuburbComponent from './component/SuburbComponent';
+import SuburbsComponent from './component/SuburbsComponent';
 
-const SUBURB = {
+const SUBURBS = [{
   name: "Alexandria",
   count: 10,
   withPrice: 9,
@@ -23,13 +23,35 @@ const SUBURB = {
   links: {
     properties: "http://localhost:7900/Alexandria/properties"
   }
-};
+},
+{
+name: "Arncliffe",
+count: 6,
+withPrice: 6,
+withStrata: 3,
+councilEstimate: 1,
+waterEstimate: 1,
+withAllCosts: 2,
+auction: 1,
+withCouncil: 1,
+maxCouncil: 326,
+minCouncil: 326,
+withWater: 1,
+maxWater: 172,
+minWater: 172,
+links: {
+properties: "http://localhost:7900/Arncliffe/properties"
+}
+},
+
+
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SuburbComponent suburb={SUBURB} />
+        <SuburbsComponent suburbs={SUBURBS} />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/component/SuburbsComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SuburbsComponent extends React.Component {
+  render() {
+    return <ul>
+        {this.props.suburbs.map((suburb) => {
+          return <li>
+          <h1>{suburb.name}</h1>
+          <h2>{suburb.count} Properties</h2>  
+          </li>
+        })}
+        
+      </ul>
+  }
+}
+
+SuburbsComponent.propTypes = {
+  suburb:PropTypes.array
+}
+
+export default SuburbsComponent;
+
