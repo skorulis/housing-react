17c186a5db87355d4c213e86dcf7763432012b32
Alex Skorulis <skorulis@gmail.com>
2018-09-05 19:08:12 +1000

    Add property image to search results

--- src/component/SearchResultComponent.js
+++ src/component/SearchResultComponent.js
@@ -10,15 +10,18 @@ class SearchResultComponent extends React.Component {
     let image = property.mainImage.server + "/320x240" + property.mainImage.uri
     let features = property.generalFeatures;
     let baseDetails = "🛏" + features.bedrooms.value + "🚿" + features.bathrooms.value + "🚗" + features.parkingSpaces.value
+    let address = property.address.streetAddress + " " + property.address.suburb
     
     return <li className="property" key={key}>
       <Columns>
         <Column isSize='1/3'>
-        <img src={image} alt="property main" />
+        <a href={property._links.prettyUrl.href}>
+          <img src={image} alt="property main" />
+        </a>
         </Column>
         <Column>
         {property.price.display}<br/>
-        {property.address.streetAddress}<br/>
+        {address}<br/>
         {baseDetails}
         </Column>
           
